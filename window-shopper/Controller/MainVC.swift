db144dd66e2b47b116f6c266c8532b92fa70d796
Willie Fatimehin <willie.fatimehin@gmail.com>
2019-01-03 21:04:44 -0500

    added data for calculator

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -12,10 +12,14 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var wageTxt: CurrencyTxtField!
     @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.6484548196, blue: 0.1462176554, alpha: 1)
         calcBtn.setTitle("Calculate", for: .normal)
@@ -24,11 +28,32 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
     
     @objc func calculate() {
-        print("we got here")
+        //makes sure that wageTxt.text and priceTxt.text is not empty
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            //convert wageTxt and priceTxt to Doubles
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                //dismiss the keyboard
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+        
     }
-
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
 }
 
